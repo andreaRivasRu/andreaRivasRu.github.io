7614f7450715cdcaaebf3b8f053a63bd7c877f08
Andrea Rivas <andrerivas25@gmail.com>
2024-03-18 11:34:32 -0300

    Estilos resultados, definicion de variables globales

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Variables globales
+
+// Client debt data
+const currency = document.getElementById('currency');
+const totalAmount = document.getElementById('total-amount');
+const annualInterestRate = document.getElementById('annual-interest-rate');
+const paymentPeriodInMonths = document.getElementById('payment-period-in-months');
+const additionalCredits = document.getElementById('additional-credits');
+
+// Show results
+const amountMonthlyPayment = document.getElementById('amount-monthly-payment');
+const totalInterest = document.getElementById('total-interest');
+const amountSaved = document.getElementById('amount-saved');
+const effectiveMonthlyRate = document.getElementById('effective-monthly-rate');
